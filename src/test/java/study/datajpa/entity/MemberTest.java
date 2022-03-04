b2c946b7b07a3e6247dacf0a2deabf10d82783cb
sangho235 <sangho235@it.gsshop.com>
2022-03-04 17:48:00 +0900

    확장기능_Web 확장 -페이징과 정렬

--- src/test/java/study/datajpa/entity/MemberTest.java
+++ src/test/java/study/datajpa/entity/MemberTest.java
@@ -1,9 +1,12 @@
 package study.datajpa.entity;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.AdditionalMatchers;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.repository.MemberRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -20,6 +23,9 @@ class MemberTest {
 
     @PersistenceContext
     EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     public void testEntity() {
@@ -48,5 +54,29 @@ class MemberTest {
             System.out.println("member = " + member);
             System.out.println("-> member.team=" + member.getTeam());
         }
+    }
+
+    @Test
+    public void JapEventBaseEntity() throws Exception {
+        //given
+        Member member = new Member("member1");
+        memberRepository.save(member);
+
+        Thread.sleep(100);
+        member.setUsername("member2");
+
+        em.flush();
+        em.clear();
+
+        //when
+        Member findMember = memberRepository.findById(member.getId()).get();
+
+        //then
+        System.out.println("findMember.createdDate = " +  findMember.getCreatedDate());
+        System.out.println( "findMember.updatedDate = " +  findMember.getLastModifiedDate());
+        System.out.println("findMember.CreatedBy = " +  findMember.getCreatedBy());
+        System.out.println("findMember.LastModifiedBy = " +  findMember.getLastModifiedBy());
+
+
     }
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -259,5 +259,11 @@ class MemberRepositoryTest {
 
     }
 
+    @Test
+    public void callCustom() {
+        List<Member> result = memberRepository.findMemberCustom();
+
+    }
+
 
 }
